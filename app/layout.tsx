5428e2ca7c32f28f8e1bab04c4a59ee0d316620f
Tim Neutkens <tim@timneutkens.nl>
2024-03-29 16:15:39 +0100

    Remove additional font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { ProgressBar, ProgressBarProvider } from "@/react-transition-progress";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <ProgressBarProvider>
           <ProgressBar className="fixed h-1 shadow-lg shadow-sky-500/20 bg-sky-500 top-0" />
           {children}
