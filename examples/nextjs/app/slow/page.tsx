6ff074858d1b24c5e6ecc52ad318c9f4d024883b
Tim Neutkens <tim@timneutkens.nl>
2024-04-02 14:17:40 +0200

    Update page.tsx

--- examples/nextjs/app/slow/page.tsx
+++ examples/nextjs/app/slow/page.tsx
@@ -4,10 +4,10 @@ import { Link } from "react-transition-progress/next";
 export default async function SlowPage() {
     unstable_noStore();
     // Introduces artificial slowdown
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     return (
         <div className="m-10">
-            <h1 className="mb-2 text-4xl font-semibold">Slow page, artificially slowed by 2 seconds</h1>
+            <h1 className="mb-2 text-4xl font-semibold">Slow page, artificially slowed by 1 second</h1>
             <Link href="/">Go to home page</Link>
         </div>
     );
