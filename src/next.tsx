26aead74358aa01b040272102dd51145426e5314
DaeHyun <qqwwee08@naver.com>
2024-05-17 12:48:24 +0900

    Add useRouter

--- src/next.tsx
+++ src/next.tsx
@@ -1,9 +1,13 @@
 "use client"
-import React from 'react'
+import React from "react"
 import { startTransition } from "react";
 import NextLink from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter as useNextRouter } from "next/navigation";
 import { useProgress } from ".";
+import type {
+    AppRouterInstance,
+    NavigateOptions,
+} from 'next/dist/shared/lib/app-router-context.shared-runtime';
 
 // Copied from  https://github.com/vercel/next.js/blob/canary/packages/next/src/client/link.tsx#L180-L191
 function isModifiedEvent(event: React.MouseEvent): boolean {
@@ -20,6 +24,31 @@ function isModifiedEvent(event: React.MouseEvent): boolean {
 }
 
 /**
+ * A custom useRouter component that wraps Next.js's next/navigation component.
+ */
+export function useRouter(): AppRouterInstance {
+    const nextRouter = useNextRouter();
+    const startProgress = useProgress();
+
+    const router: AppRouterInstance = {
+        ...nextRouter,
+        push: (href: string, options?: NavigateOptions) => {
+            startTransition(() => {
+                startProgress();
+                nextRouter.push(href, options);
+            });
+        },
+        replace(href: string, options?: NavigateOptions) {
+            startTransition(() => {
+                startProgress();
+                nextRouter.replace(href, options);
+            });
+        },
+    };
+    return router;
+}
+
+/**
  * A custom Link component that wraps Next.js's next/link component.
  */
 export function Link({
@@ -29,7 +58,6 @@ export function Link({
     ...rest
 }: Parameters<typeof NextLink>[0]) {
     const router = useRouter();
-    const startProgress = useProgress()
 
     return (
         <NextLink
@@ -37,15 +65,12 @@ export function Link({
             onClick={(e) => {
                 if (isModifiedEvent(e)) return;
                 e.preventDefault();
-                startTransition(() => {
-                    startProgress()
-                    const url = href.toString()
-                    if (replace) {
-                        router.replace(url)
-                    } else {
-                        router.push(url)
-                    }
-                })
+                const url = href.toString()
+                if (replace) {
+                    router.replace(url)
+                } else {
+                    router.push(url)
+                }
             }}
             {...rest}
         >
